a7ab9ccae05fb36c91d8a464a88c3cbaf9e7514b
Chinnadurai R <chinnadurai.r@engagedly.com>
2025-09-28 11:14:32 +0530

    Create github actions

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket       = "terraform-backend-chinna-123"
     key          = "terraform/terraform.tfstate"
-    region       = "us-west-2"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
